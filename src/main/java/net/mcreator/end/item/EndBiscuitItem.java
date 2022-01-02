11921e8105a1275a6c2c92dd766bfe938e65053f
Redranger19 <thoma@DESKTOP-1I8IG04.localdomain>
2022-01-02 14:49:54 -0600

    Biscuit Update

--- src/main/java/net/mcreator/end/item/EndBiscuitItem.java
+++ src/main/java/net/mcreator/end/item/EndBiscuitItem.java
@@ -1,9 +1,21 @@
 
 package net.mcreator.end.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+import net.mcreator.end.EndModElements;
+
 @EndModElements.ModElement.Tag
 public class EndBiscuitItem extends EndModElements.ModElement {
-
 	@ObjectHolder("end:end_biscuit")
 	public static final Item block = null;
 
@@ -17,20 +29,27 @@ public class EndBiscuitItem extends EndModElements.ModElement {
 	}
 
 	public static class FoodItemCustom extends Item {
-
 		public FoodItemCustom() {
-			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
-					.food((new Food.Builder()).hunger(4).saturation(0.3f)
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.EPIC).food((new Food.Builder()).hunger(20).saturation(1f)
 
-							.build()));
+					.build()));
 			setRegistryName("end_biscuit");
+		}
+
+		@Override
+		public int getUseDuration(ItemStack stack) {
+			return 20;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
 		}
 
 		@Override
 		public UseAction getUseAction(ItemStack itemstack) {
 			return UseAction.EAT;
 		}
-
 	}
-
 }
